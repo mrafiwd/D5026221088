9983b4615dec77df7e939707f620f0c6aab88b0b
mrafiwd <mrafi.wd@gmail.com>
2023-12-04 00:16:35 +0700

    Tugas Pertemuan 27 November 2023

--- resources/views/tambahPegawai.blade.php
+++ resources/views/tambahPegawai.blade.php
@@ -20,25 +20,25 @@
         <div class = "form-group">
             <label for = "nama" class = "col-sm-2 control-label">Nama</label>
             <div class = "col-sm-10">
-               <input  name="nama" type = "text" class = "form-control" id = "nama" placeholder = "Masukan Nama">
+               <input  name="nama" type = "text" required = "required" class = "form-control" id = "nama" placeholder = "Masukan Nama">
             </div>
          </div>
          <div class = "form-group">
             <label for = "jabatan" class = "col-sm-2 control-label">Jabatan</label>
             <div class = "col-sm-10">
-               <input  name="jabatan" type = "text" class = "form-control" id = "jabatan" placeholder = "Masukan Jabatan">
+               <input  name="jabatan" type = "text" required = "required" class = "form-control" id = "jabatan" placeholder = "Masukan Jabatan">
             </div>
          </div>
          <div class = "form-group">
             <label for = "umur" class = "col-sm-2 control-label">Umur</label>
             <div class = "col-sm-10">
-               <input name = "umur" type = "text" class = "form-control" id = "nama" placeholder = "Masukan Umur">
+               <input name = "umur" type = "number"  required = "required" class = "form-control" id = "umur" placeholder = "Masukan Umur">
             </div>
          </div>
          <div class = "form-group">
             <label for = "alamat" class = "col-sm-2 control-label">Alamat</label>
             <div class = "col-sm-10">
-               <textarea name="alamay" class = "form-control" id = "alamat" placeholder = "Masukan Alamat"></textarea>
+                <textarea required="required" name="alamat" class="form-control" id="alamat" placeholder="Masukan Alamat"></textarea>
             </div>
          </div>
         <br>
